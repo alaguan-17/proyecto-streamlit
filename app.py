7987ee2f443645a977635389daa72be84c9b24d4
alaguan-17 <00051019@uca.edu.sv>
2025-01-04 00:20:20 -0600

    k12

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
-from PIL import Image
 
-# Configuración inicial: debe ser la primera instrucción de Streamlit
+# Configuración inicial de la página principal
 st.set_page_config(
     page_title="Airbnb Analytics",
     layout="wide",
@@ -15,20 +14,14 @@ st.markdown("Un análisis interactivo para entender las tendencias y factores cl
 
 # Menú de navegación
 menu_options = {
-    "EDA": "streamlit_app/Pages/1_EDA.py",
+    "Exploración de Datos (EDA)": "streamlit_app/Pages/1_EDA.py",
     "Hipótesis": "streamlit_app/Pages/2_HIPOTESIS.py",
     "Modelos": "streamlit_app/Pages/3_MODELO.py"
 }
 
 menu = st.sidebar.radio(
     "Navega por las secciones:",
-    options=list(menu_options.keys())
+    options=list(menu_options.keys()),
+    format_func=lambda x: x
 )
 
-# Cargar y redirigir a la página seleccionada
-if menu in menu_options:
-    with open(menu_options[menu], encoding="utf-8") as file:
-        exec(file.read(), globals())
-
-# Footer
-st.sidebar.markdown("👨‍💻 **GRUPO UCA** | 🌐 [PROYECTO INTEGRADOR]")
